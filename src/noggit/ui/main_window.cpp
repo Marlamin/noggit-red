2395ce5522c500e060ceebaa779c3a8f0b36c69d
Skarn <skarnproject@gmail.com>
2020-11-05 17:46:33 +0300

    add missing file

--- src/noggit/ui/main_window.cpp
+++ src/noggit/ui/main_window.cpp
@@ -63,6 +63,7 @@ namespace noggit
       _settings = new settings(this);
 
       _menuBar = menuBar();
+      _menuBar->setNativeMenuBar(false);
       titleBarWidget.horizontalLayout->insertWidget(1, _menuBar);
       titleBarWidget.horizontalLayout->insertWidget(1, new QLabel(title.str().c_str(), this));
       _menuBar->setMaximumHeight(20);
