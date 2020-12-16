356856a743e58b903a50466536d2c2d177ff6ba1
sshumakov3 <sshumakov3@gmail.com>
2020-12-16 18:42:58 +0300

    nodes: implement return

---
+++ src/noggit/Red/PresetEditor/Nodes/LogicReturnNoDataNode.hpp
@@ -0,0 +1,35 @@
+// This file is part of Noggit3, licensed under GNU General Public License (version 3).
+
+#ifndef NOGGIT_LOGICRETURNNODATANODE_HPP
+#define NOGGIT_LOGICRETURNNODATANODE_HPP
+
+#include "LogicNodeBase.hpp"
+
+using QtNodes::PortType;
+using QtNodes::PortIndex;
+using QtNodes::NodeData;
+using QtNodes::NodeDataType;
+using QtNodes::NodeDataModel;
+using QtNodes::NodeValidationState;
+
+
+namespace noggit
+{
+    namespace Red::PresetEditor::Nodes
+    {
+        class LogicReturnNoDataNode : public LogicNodeBase
+        {
+        Q_OBJECT
+
+        public:
+            LogicReturnNoDataNode();
+            NodeValidationState validate() override;
+            void compute() override;
+
+        };
+
+    }
+
+}
+
+#endif //NOGGIT_LOGICRETURNNODATANODE_HPP
