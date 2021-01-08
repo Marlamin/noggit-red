22f720160708bd416b709099ab76a29aff16a4f1
sshumakov3 <sshumakov3@gmail.com>
2021-01-08 13:30:06 +0300

    fix warnings

--- src/noggit/Red/NodeEditor/Nodes/VectorMathNode.hpp
+++ src/noggit/Red/NodeEditor/Nodes/VectorMathNode.hpp
@@ -24,10 +24,111 @@ namespace noggit
       {
 
       public:
-        VectorMathNodeBase();
-        void compute() override;
-        QJsonObject save() const override;
-        void restore(QJsonObject const& json_obj) override;
+        VectorMathNodeBase()
+        {
+           setCaption("Add");
+          _validation_state = NodeValidationState::Valid;
+
+          _operation = new QComboBox(&_embedded_widget);
+
+          if constexpr (std::is_same<T, glm::vec3>::value)
+          {
+            _operation->addItems({"Add", "Subtract", "Multiply", "Divide", "Cross"});
+          }
+          else
+          {
+            _operation->addItems({"Add", "Subtract", "Multiply", "Divide"});
+          }
+
+
+          QComboBox::connect(_operation, qOverload<int>(&QComboBox::currentIndexChanged)
+              ,[this](int index)
+             {
+                 setCaption(_operation->currentText());
+             }
+          );
+
+          std::string type_name;
+
+          if constexpr (std::is_same<T, glm::vec3>::value)
+          {
+            type_name = "Vector3D";
+          }
+          else if constexpr (std::is_same<T, glm::vec4>::value)
+          {
+            type_name = "Vector4D";
+          }
+          else
+          {
+            type_name = "Vector2D";
+          }
+
+          addWidgetTop(_operation);
+
+          addPort<T>(PortType::In, type_name.c_str(), true);
+          addDefaultWidget(_in_ports[0].data_type->default_widget(&_embedded_widget), PortType::In, 0);
+
+          addPort<T>(PortType::In, type_name.c_str(), true);
+          addDefaultWidget(_in_ports[1].data_type->default_widget(&_embedded_widget), PortType::In, 1);
+
+          addPort<T>(PortType::Out, type_name.c_str(), true);
+        };
+
+        void compute() override
+        {
+          auto vector_1_ptr = static_cast<T*>(_in_ports[0].in_value.lock().get());
+          T1 vector_1 = vector_1_ptr ? vector_1_ptr->value()
+              : static_cast<T*>(_in_ports[0].data_type->default_widget_data(_in_ports[0].default_widget).get())->value();
+
+          auto vector_2_ptr = static_cast<T*>(_in_ports[1].in_value.lock().get());
+          T1 vector_2 = vector_2_ptr ? vector_2_ptr->value()
+              : static_cast<T*>(_in_ports[1].data_type->default_widget_data(_in_ports[1].default_widget).get())->value();
+
+          T1 result;
+          switch (_operation->currentIndex())
+          {
+            case 0: // Add
+              result = vector_1 + vector_2;
+              break;
+            case 1: // Subtract
+              result = vector_1 - vector_2;
+              break;
+            case 2: // Multiply
+              result = vector_1 * vector_2;
+              break;
+            case 3: // Divide
+              result = vector_1 / vector_2;
+              break;
+            case 4: // Cross
+              if constexpr (std::is_same<T, glm::vec3>::value)
+              {
+                result = glm::cross(vector_1, vector_2);
+              }
+              break;
+          }
+
+          _out_ports[0].out_value = std::make_shared<T>(result);
+          Q_EMIT dataUpdated(0);
+        };
+
+        QJsonObject save() const override
+        {
+          QJsonObject json_obj = BaseNode::save();
+
+          json_obj["operation"] = _operation->currentIndex();
+          _in_ports[0].data_type->to_json(_in_ports[0].default_widget, json_obj, "vector_1");
+          _in_ports[1].data_type->to_json(_in_ports[1].default_widget, json_obj, "vector_2");
+
+          return json_obj;
+        }
+
+        void restore(QJsonObject const& json_obj) override
+        {
+          _in_ports[0].data_type->from_json(_in_ports[0].default_widget, json_obj, "vector_1");
+          _in_ports[1].data_type->from_json(_in_ports[1].default_widget, json_obj, "vector_2");
+          _operation->setCurrentIndex(json_obj["operation"].toInt());
+          BaseNode::restore(json_obj);
+        };
 
       protected:
         QComboBox* _operation;
--- src/noggit/Red/NodeEditor/Nodes/VectorScalarMathNode.hpp
+++ src/noggit/Red/NodeEditor/Nodes/VectorScalarMathNode.hpp
@@ -25,10 +25,90 @@ namespace noggit
       {
 
       public:
-        VectorScalarMathNodeBase();
-        void compute() override;
-        QJsonObject save() const override;
-        void restore(QJsonObject const& json_obj) override;
+        VectorScalarMathNodeBase()
+        {
+          setCaption("Dot");
+          _validation_state = NodeValidationState::Valid;
+
+          _operation = new QComboBox(&_embedded_widget);
+          _operation->addItems({"Dot", "Distance"});
+
+          QComboBox::connect(_operation, qOverload<int>(&QComboBox::currentIndexChanged)
+              ,[this](int index)
+                             {
+                                 setCaption(_operation->currentText());
+                             }
+          );
+
+          std::string type_name;
+
+          if constexpr (std::is_same<T, glm::vec3>::value)
+          {
+            type_name = "Vector3D";
+          }
+          else if constexpr (std::is_same<T, glm::vec4>::value)
+          {
+            type_name = "Vector4D";
+          }
+          else
+          {
+            type_name = "Vector2D";
+          }
+
+          addWidgetTop(_operation);
+
+          addPort<T>(PortType::In, type_name.c_str(), true);
+          addDefaultWidget(_in_ports[0].data_type->default_widget(&_embedded_widget), PortType::In, 0);
+          addPort<T>(PortType::In, type_name.c_str(), true);
+          addDefaultWidget(_in_ports[1].data_type->default_widget(&_embedded_widget), PortType::In, 1);
+
+          addPort<DecimalData>(PortType::Out, "Decimal", true);
+        };
+
+        void compute() override
+        {
+          auto vector_1_ptr = static_cast<T*>(_in_ports[0].in_value.lock().get());
+          T1 vector_1 = vector_1_ptr ? vector_1_ptr->value()
+                                     : static_cast<T*>(_in_ports[0].data_type->default_widget_data(_in_ports[0].default_widget).get())->value();
+
+          auto vector_2_ptr = static_cast<T*>(_in_ports[1].in_value.lock().get());
+          T1 vector_2 = vector_2_ptr ? vector_2_ptr->value()
+                                     : static_cast<T*>(_in_ports[1].data_type->default_widget_data(_in_ports[1].default_widget).get())->value();
+
+          double result;
+          switch (_operation->currentIndex())
+          {
+            case 0: // Dot
+              result = glm::dot(vector_1, vector_2);
+              break;
+            case 1: // Distance
+              result = (vector_1 - vector_2).length();
+              break;
+          }
+
+          _out_ports[0].out_value = std::make_shared<DecimalData>(result);
+          Q_EMIT dataUpdated(0);
+        };
+
+        QJsonObject save() const override
+        {
+          QJsonObject json_obj = BaseNode::save();
+
+          json_obj["operation"] = _operation->currentIndex();
+          _in_ports[0].data_type->to_json(_in_ports[0].default_widget, json_obj, "vector_1");
+          _in_ports[1].data_type->to_json(_in_ports[1].default_widget, json_obj, "vector_2");
+
+          return json_obj;
+
+        };
+
+        void restore(QJsonObject const& json_obj) override
+        {
+          _in_ports[0].data_type->from_json(_in_ports[0].default_widget, json_obj, "vector_1");
+          _in_ports[1].data_type->from_json(_in_ports[1].default_widget, json_obj, "vector_2");
+          _operation->setCurrentIndex(json_obj["operation"].toInt());
+          BaseNode::restore(json_obj);
+        };
 
       private:
         QComboBox* _operation;
