85356d54bc6f64bcddcc37c9ef08d181ed31eae8
sshumakov3 <sshumakov3@gmail.com>
2021-01-10 13:44:23 +0300

    add missing files

---
+++ src/noggit/Red/NodeEditor/Nodes/Functions/PrintNode.hpp
@@ -0,0 +1,39 @@
+#ifndef NOGGIT_PRINTNODE_HPP
+#define NOGGIT_PRINTNODE_HPP
+
+#include "noggit/Red/NodeEditor/Nodes/LogicNodeBase.hpp"
+#include <QLineEdit>
+
+using QtNodes::PortType;
+using QtNodes::PortIndex;
+using QtNodes::NodeData;
+using QtNodes::NodeDataType;
+using QtNodes::NodeDataModel;
+using QtNodes::NodeValidationState;
+
+
+namespace noggit
+{
+    namespace Red::NodeEditor::Nodes
+    {
+        class PrintNode : public LogicNodeBase
+        {
+        Q_OBJECT
+
+        public:
+            PrintNode();
+            void compute() override;
+            NodeValidationState validate() override;
+            QJsonObject save() const override;
+            void restore(QJsonObject const& json_obj) override;
+
+        private:
+            QLineEdit* _text;
+
+        };
+
+    }
+
+}
+
+#endif //NOGGIT_PRINTNODE_HPP
