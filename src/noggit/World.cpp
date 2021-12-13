ffdbca3b9e141ba31efbe77a62e603864b7594b2
Alister <alister.greg@gmail.com>
2021-12-13 14:18:44 +0000

    removing pool allocators
    this could potentially have a performance impact. gave it a quick test and I couldn't notice a difference

--- src/noggit/World.cpp
+++ src/noggit/World.cpp
@@ -30,7 +30,6 @@
 #include <external/tracy/Tracy.hpp>
 
 #include <boost/format.hpp>
-#include <boost/pool/pool_alloc.hpp>
 
 #include <QtWidgets/QMessageBox>
 #include <QDir>
@@ -1381,8 +1380,8 @@ void World::draw (glm::mat4x4 const& model_view
 
   std::unordered_map<Model*, std::size_t> model_with_particles;
 
-  tsl::robin_map<Model*, std::vector<glm::mat4x4, boost::pool_allocator<glm::mat4x4>>> models_to_draw;
-  std::vector<WMOInstance*, boost::pool_allocator<WMOInstance*>> wmos_to_draw;
+  tsl::robin_map<Model*, std::vector<glm::mat4x4>> models_to_draw;
+  std::vector<WMOInstance*> wmos_to_draw;
 
   static int frame = 0;
 
@@ -1689,12 +1688,7 @@ void World::draw (glm::mat4x4 const& model_view
 
     models_to_draw.clear();
     wmos_to_draw.clear();
-    boost::singleton_pool<boost::pool_allocator_tag
-    , sizeof(std::vector<glm::mat4x4, boost::pool_allocator<glm::mat4x4>>)>::purge_memory();
-
-    boost::singleton_pool<boost::pool_allocator_tag
-        , sizeof(std::vector<WMOInstance*, boost::pool_allocator<WMOInstance*>>)>::purge_memory();
-
+   
     if(draw_models_with_box || (draw_hidden_models && !model_boxes_to_draw.empty()))
     {
       OpenGL::Scoped::use_program m2_box_shader{ *_m2_box_program.get() };
