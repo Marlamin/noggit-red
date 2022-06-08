50b95f1b7220228579ba65cb88c06d80a39d7788
T1ti <alkioxbf3@gmail.com>
2022-06-08 21:12:45 +0200

    initialise WDT data for new maps

--- src/noggit/ui/tools/MapCreationWizard/Ui/MapCreationWizard.cpp
+++ src/noggit/ui/tools/MapCreationWizard/Ui/MapCreationWizard.cpp
@@ -470,6 +470,7 @@ void MapCreationWizard::saveCurrentEntry()
   record.writeString(1, _directory->text().toStdString());
 
   record.write(2, _instance_type->itemData(_instance_type->currentIndex()).toInt());
+  record.write(3, _sort_by_size_cat->isChecked() ? 16 : 0 );
   _map_name->toRecord(record, 5);
 
   record.write(22, _area_table_id->value());
