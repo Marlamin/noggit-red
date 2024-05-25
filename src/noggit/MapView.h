1561fbd7c36a0520c845cd4648e8294bbccc2eea
T1ti <40864460+T1ti@users.noreply.github.com>
2024-05-25 07:10:50 +0200

    display chunk alphamaps in texture picker

--- src/noggit/MapView.h
+++ src/noggit/MapView.h
@@ -206,6 +206,8 @@ private:
   std::vector<selection_type> lastSelected;
 
   bool _rotation_editor_need_update = false;
+  bool _texture_picker_need_update = false;
+  bool _area_picker_need_update = false;
 
   // Vars for the ground editing toggle mode store the status of some
   // view settings when the ground editing mode is switched on to
