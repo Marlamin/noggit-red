aee8a9bdf49070895a461ea3988649bc478f9998
Martin Benjamins <ictmartin@gmail.com>
2024-06-29 21:49:17 +0200

    Hook up modern feature setting

--- src/noggit/ui/windows/settingsPanel/SettingsPanel.cpp
+++ src/noggit/ui/windows/settingsPanel/SettingsPanel.cpp
@@ -201,6 +201,7 @@ namespace Noggit
           _settings->value("additional_file_loading_log", false).toBool());
       ui->_keyboard_locale->setCurrentText(_settings->value("keyboard_locale", "QWERTY").toString());
       ui->_theme->setCurrentText(_settings->value("theme", "Dark").toString());
+      ui->_modern_features->setChecked(_settings->value("modern_features", false).toBool());
 
       ui->assetBrowserBgCol->setColor(_settings->value("assetBrowser/background_color",
         QVariant::fromValue(QColor(127, 127, 127))).value<QColor>());
@@ -284,6 +285,7 @@ namespace Noggit
       _settings->setValue("systemWindowFrame", ui->_systemWindowFrame->isChecked());
       _settings->setValue("nativeMenubar", ui->_nativeMenubar->isChecked());
       _settings->setValue("classicUI", ui->_classic_ui->isChecked());
+      _settings->setValue("modern_features", ui->_modern_features->isChecked());
 
 #ifdef USE_MYSQL_UID_STORAGE
       _settings->setValue ("project/mysql/enabled", ui->MySQL_box->isChecked());
