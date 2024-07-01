beff88dc526086ebaa2d9a0b4a846c8aa5536c0c
T1ti <40864460+T1ti@users.noreply.github.com>
2024-07-01 22:35:10 +0200

    improve ground effect tool UI

--- src/noggit/ui/tools/AssetBrowser/Ui/AssetBrowser.hpp
+++ src/noggit/ui/tools/AssetBrowser/Ui/AssetBrowser.hpp
@@ -109,7 +109,7 @@ namespace Noggit
       // commented objects that shouldn't be placed on the map, still accessible through Show all
       const QMap<QString, asset_browse_mode> brosweModeLabels =  {
       { "World Objects", asset_browse_mode::world },
-      // { "Detail Doodads", asset_browse_mode::detail_doodads },
+      { "Detail Doodads", asset_browse_mode::detail_doodads },
       // { "Skybox", asset_browse_mode::skybox },
       // { "Creatures", asset_browse_mode::creatures },
       // { "Characters", asset_browse_mode::characters },
