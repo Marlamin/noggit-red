c92eeb7ec2e9a18551d9c8520ba7c8493d453392
Martin Benjamins <ictmartin@gmail.com>
2024-07-24 03:25:53 +0200

    Restore our logic for maptexture DXT5/DXT1 mipmaps

--- src/noggit/rendering/WorldRender.cpp
+++ src/noggit/rendering/WorldRender.cpp
@@ -1772,7 +1772,7 @@ bool WorldRender::saveMinimap(TileIndex const& tile_idx, MinimapRenderSettings* 
       uint32_t file_size;
       // void* blp_image = blp.createBlpDxtInMemory(true, FORMAT_DXT5, file_size);
       // this mirrors blizzards : dxt1, no mipmap
-      void* blp_image = blp.createBlpDxtInMemory(false, FORMAT_DXT1, file_size);
+      void* blp_image = blp.createBlpDxtInMemory(settings->file_format == ".blp (DXT5)" ? true : false, settings->file_format == ".blp (DXT5)" ? FORMAT_DXT5 : FORMAT_DXT1, file_size);
 
       // converts the texture name to an md5 hash like blizzard, this is used to avoid duplicates textures for ocean
       // downside is that if the file gets updated regularly there will be a lot of duplicates in the project folder
